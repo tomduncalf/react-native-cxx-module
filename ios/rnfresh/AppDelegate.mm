6ddce715d8fcdd6c6117de91ea36ae4c047e6d6c
Tom Duncalf <tom@tomduncalf.com>
2018-03-22 17:05:20 +0100

    Add some comments

--- ios/rnfresh/AppDelegate.mm
+++ ios/rnfresh/AppDelegate.mm
@@ -9,6 +9,8 @@
 
 #import "AppDelegate.h"
 
+#import "ReactNative/TestCxxRCTModule.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -21,6 +23,16 @@
                                                    moduleName:@"TestCxx"
                                             initialProperties:nil];
   
+  TestCxxModule* module = [bridge moduleForName:@"TestCxxModule"];
+  // 1. Is there a better way to cause the module to lazily initialize itself before I want to use it?
+  [module methodsToExport];
+  
+  // 2. `module` now contains the TestCxxModule Obj-C wrapper, but how do I get the actual C++ module it wraps?
+  // Should I just keep my own reference to it inside my wrapper, or is there a proper way to do get it?
+  
+  // 3. Even if I can access the actual C++ module inside, its instance_ property is null (put a breakpoint here to see)
+  // - is this correct? If so, how can I access a React Native instance to callback to JS from C++?
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
